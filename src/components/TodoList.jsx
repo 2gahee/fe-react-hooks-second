a29f106eb9dffca8dad7c8911d5511c258c2ab02
unknown <lgh7956@gmail.com>
2023-04-28 20:46:25 +0900

    0428 가희 과제

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -4,37 +4,50 @@ function TodoList() {
   const [todos, setTodos] = useState([]); // todo list를 담는 state, 초기 값은 빈 배열
   const [inputValue, setInputValue] = useState(""); // 입력한 todo를 담는 state, 초기 값은 빈 문자열
   const [count, setCount] = useState(0); // 완료한 todo의 개수를 담는 state, 초기 값은 0
-
+  
   useEffect(() => {
     const data = [
+      {id: 1, text: '산책가기', completed: false}, {id: 2, text: '멋사과제', completed: true}, {id: 3, text: '책읽기', completed: false}, {id: 4, text: '야구보기', completed: false}
       // 이곳에 초기 렌더링 시 표시 될 '객체 형태의 할 일들'을 작성해주세요. (hint: 객체의 key는 id, text, completed 입니다.)
     ];
     // 생성한 todo를 state의 상태 변수인 todos 에 저장해주세요
+    setTodos(data);
   }, []);
 
-  useEffect(() => {
+  useEffect(() => { 
+    let n = 0;
+    for(var i = 0; i<todos.length;i++){
+      if(todos[i].completed===true){
+        n++;
+      }
+    }
+    setCount(n);
     // 이곳에 todos에 변화가 생길 때마다 완료한 할 일의 개수(count)를 'update' 하도록 하는 코드를 작성해주세요. (+ dependency array 에는 어떤 값이 들어가야 할까요?)
-  }, []);
+  }, [todos]);
 
   useEffect(() => {
+    if (todos.length===count && count>0){
+      alert("오늘 할 일을모두 완료하셨습니다")
+    }
     // 이곳에 count의 update를 감지하면서 모든 할 일 모두 완료했을 때 "오늘 할 일을 모두 완료하셨네요!"를 출력하는 알림창이 뜨도록 코드를 작성해주세요. (+ dependency array 에는 어떤 값이 들어가야 할까요?)
-  }, []);
+  }, [count]);
 
   const handleInput = (e) => {
+    setInputValue(e.target.value)
     // 이곳에 입력창에 입력한 값이 state의 상태 변수인 inputValue에 저장되도록 코드를 작성해주세요.
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const newTodo = {id: Date.now() /* 이곳에 새로운 todo의 text와 completed 속성을 설정하도록 코드를 작성해주세요 */};
-    setTodos(/* 이곳에 기존의 todos에 새로 생성된 newTodo를 추가하도록 하는 코드를 작성해주세요 (hint: 배열에서의 spread 연산자 활용) */);
+    const newTodo = {id: Date.now(),  text: inputValue, completed: false /* 이곳에 새로운 todo의 text와 completed 속성을 설정하도록 코드를 작성해주세요 */};
+    setTodos([...todos, newTodo] /* 이곳에 기존의 todos에 새로 생성된 newTodo를 추가하도록 하는 코드를 작성해주세요 (hint: 배열에서의 spread 연산자 활용) */);
     setInputValue("");
   };
 
   const handleTodo = (id) => {
     const updatedTodos = todos.map((todo) => {
       if (todo.id === id) {
-        return {
+        return { ...todo,completed: !todo.completed
           /* 이곳에 기존의 todo의 객체 속성에서 completed 객체의 속성만 변경되도록 하는 코드를 작성해주세요 (hint: 객체에서의 spread 연산자 활용) */
         };
       }
